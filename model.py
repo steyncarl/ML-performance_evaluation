879ad2ca542aa9dc61598eaabd9efb2c9c66879f
Cedric Oeldorf <cedric.oeldorf@yahoo.de>
2016-12-04 16:59:47 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -2,28 +2,27 @@
 """
 Created on Sat Dec  3 17:14:46 2016
 
-@author: Carl Steyn
+@author: Carl Penis
 """
 import numpy as np
-import os
 from sklearn import svm
-os.chdir('C:\\Carl\\Work\\NumerAI')
+import pandas as pd
 
-#from numpy import genfromtxt
-#data = genfromtxt('numerai_training_data.csv', delimiter=',')
-#data = data[1:,1:]
-#np.save('data',data)
 
-data = np.load('data.npy')
+data = pd.read_csv("/home/cedric/Documents/Projects/Numerai/November/data/train.csv")
+data = np.array(data)
 X = data[:,0:20]
-y = data[:,20]
+y = data[:,21]
 scalar = 0.6
 n = int(scalar*len(X))
 X_train = X[0:n,:]
 X_test = X[n+1:,:]
 y_train = y[0:n]
 y_test = y[n+1:]
-#%%
-clf = svm.SVC(degree=2,probability=True,class_weight='balanced')
+
+clf = svm.SVC(degree       = 2, 
+              probability  = True, 
+              class_weight = 'balanced', 
+              verbose      = True)
 clf.fit(X_train,y_train)
 clf.score(X_test,y_test)
